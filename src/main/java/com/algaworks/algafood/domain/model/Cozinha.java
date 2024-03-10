b10dfca233f72faecf91b984e9b36d2b8698458b
Igor <igoralbuquerque021@gmail.com>
2024-03-09 21:48:47 -0300

    chore: configuração de propriedades jpa/hibernate

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -2,20 +2,21 @@ package com.algaworks.algafood.domain.model;
 
 import java.util.Objects;
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nom_cozinha")
-	private Long nome;
+	private String nome;
 
 	public Long getId() {
 		return id;
@@ -25,11 +26,11 @@ public class Cozinha {
 		this.id = id;
 	}
 
-	public Long getNome() {
+	public String getNome() {
 		return nome;
 	}
 
-	public void setNome(Long nome) {
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
