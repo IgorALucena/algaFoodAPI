336bbb1a6b89e73d2d4d362c4ca7074918ca745e
Igor <igoralbuquerque021@gmail.com>
2024-03-26 19:44:51 -0300

    feat: recurso/controller singleton - busca por id

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class CozinhaController {
 	public List<Cozinha> listar(){//coleção de recursos
 		return repository.listar();
 		
+	}
+	
+	@GetMapping(value = "/{cozinhaId}")// recurso singleton
+	public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
+		return repository.buscar(id);
 	}
 
 }
